64b75530ff21078063403d2e686d5fd164d40286
timothy verhaeghe <timothy@taglayer.com>
2017-10-26 21:27:33 +0200

    updated test

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const pelican = require('./index');
+
 const array = [1, 2, 3, 4, 5];
-pelican.forEach(array, function(number, cb){
+pelican.forEach(array, (number, cb) => {
   console.log(number);
   cb(); // call to go to the next object.
-}, function(){
+}, () => {
   console.log('Done');
-})
+});
